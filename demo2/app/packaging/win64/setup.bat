66c712cd936a98b32788f31cd2e2556747387091
broquet <broquet@esrf.fr>
2017-12-15 06:51:55 +0100

    demo2/packaging: remove bootnodes to try startup with static-nodes.json file

--- demo2/app/packaging/win64/setup.bat
+++ demo2/app/packaging/win64/setup.bat
@@ -23,4 +23,4 @@ echo "---------"
 start app/index.html
 
 :: start node
-.\geth.exe --datadir %DATADIR% --identity %HOSTNAME% --mine --minerthreads=1 --maxpeers 100 --networkid 170788 --rpcapi "db,personal,admin,eth,net,web3,miner" --rpc --rpccorsdomain "*" --nat extip:192.168.1.* --bootnodes "enode://b81413b112e315ed8d6ec7c6ce22d7146b803521599f55f6da39803f4a0e9f05235c1632fc2be2093c215d7be343fe0d864a1cdb497ad1f7da5a9ab36f674a17@192.168.1.1:30303"
+.\geth.exe --datadir %DATADIR% --identity %HOSTNAME% --mine --minerthreads=1 --maxpeers 100 --networkid 170788 --rpcapi "db,personal,admin,eth,net,web3,miner" --rpc --rpccorsdomain "*"
