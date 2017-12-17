a9febe7fba2ac2b345e1a28e9f765b7f8470dd9a
broquet <broquet@esrf.fr>
2017-12-17 23:06:58 +0100

    demo2/deployment: simpler startnode script

--- demo2/deployment/startnode.sh
+++ demo2/deployment/startnode.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-#Description: Script starting the node. Input arg: 1=mining, otherwise no mining
+#Description: Script starting the node
 
-
-function help() {
-    echo "Usage: $0 <mining>"
-    echo "mining:"
-    echo "1 -> start the node mining"
-    echo "otherwise -> start the node no mining"
-    echo "Example: $0 1"
-}
-
-if [ $# -ne 1 ]
-then
-    help
-    exit 1
-fi
-
-if [ "$1" == "1" ]
-then
-    geth --datadir $PWD/data/ --identity $HOSTNAME --mine --maxpeers 100 --networkid 170788 --rpcapi personal,admin,eth,net,web3,miner --rpc --rpccorsdomain "*" --nat any --nodekey $PWD/data/bootkey_$HOSTNAME.key console
-else
-    geth --datadir $PWD/data/ --identity $HOSTNAME --maxpeers 100 --networkid 170788 --rpcapi personal,admin,eth,net,web3,miner --rpc --rpccorsdomain "*" --nat any --nodekey $PWD/data/bootkey_$HOSTNAME.key console
-fi
+geth --datadir $PWD/data/ --identity $HOSTNAME --mine --minerthreads 1 --maxpeers 100 --networkid 170788 --rpcapi personal,admin,eth,net,web3,miner --rpc --rpccorsdomain "*" --nat extip:192.168.1.1 --nodekey $PWD/data/bootkey_$HOSTNAME.key console
